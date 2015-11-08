55b13a0bc2b3d0540d81b09a4c8fae89372d7771
sabarish14 <hadoop@ip-172-31-0-60.us-west-2.compute.internal>
2015-11-08 00:31:50 +0000

    Adding sample project

---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+sudo yum install -y apache-maven
+sudo yum install -y git
